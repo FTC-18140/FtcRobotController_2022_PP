7d671e912b254765f75209f72bdd1af5579cc324
Kristopher Lee <bacon.101@icloud.com>
2023-01-23 21:15:27 -0600

    Adding a place to try a new Telop class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandOpModes/FTCLIb_Teleop.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.CommandOpModes;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+public class FTCLIb_Teleop extends TBDOpModeBase
+{
+    GamepadEx pad1 = new GamepadEx( gamepad1 );
+    GamepadEx pad2 = new GamepadEx( gamepad2 );
+
+
+
+}
