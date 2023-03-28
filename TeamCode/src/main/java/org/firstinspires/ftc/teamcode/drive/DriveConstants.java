377ebb9ec761512afeaab608ede87432ee79a288
Kristopher Lee <6152682+kalee1@users.noreply.github.com>
2023-03-28 16:46:37 -0500

    Reduced the max speed, accel, ang vel, ang accel.  Just want it to start more slowly.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -98,10 +98,10 @@ public class DriveConstants {
      * https://github.com/acmerobotics/road-runner-quickstart/issues/91
 
      */
-    public static double MAX_VEL = 40;
-    public static double MAX_ACCEL = 40;
-    public static double MAX_ANG_VEL = Math.toRadians(180);
-    public static double MAX_ANG_ACCEL = Math.toRadians(180);
+    public static double MAX_VEL = 30;
+    public static double MAX_ACCEL = 30;
+    public static double MAX_ANG_VEL = Math.toRadians(120);
+    public static double MAX_ANG_ACCEL = Math.toRadians(120);
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
