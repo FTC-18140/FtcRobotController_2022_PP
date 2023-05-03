c38f716e5eb3017e888e178565807e7249326ddd
River <jeremy.gbsn@gmail.com>
2023-05-03 17:43:32 -0500

    Testing in Auto.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RiverTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RiverTest1.java
@@ -3,16 +3,25 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
 @Autonomous
 public class RiverTest1 extends OpMode {
     Thunderbot_2022 robot = new Thunderbot_2022();
     boolean done = false;
+    double power = 0.4;
 
-    double power = 0.2;
-    double distance = 45;
-    double heading = 0;
+    int step = 0;
+
+
+
+    double[] steps = {
+            20,
+            -45,
+            20,
+            0.4,
+            -20,
+            45,
+            1
+    };
 
     @Override
     public void init()
@@ -31,11 +40,82 @@ public class RiverTest1 extends OpMode {
     @Override
     public void loop()
     {
-        if(!done){
-            done = robot.gyroDrive(heading, distance, power);
-        }else{
+        if(step >= steps.length-1){
             robot.stop();
+        }else {
+            switch (step) {
+                case (0):
+                    if(!done){
+                        done = robot.drive(0, steps[step], power) && robot.linearSlide.clawMove(steps[6]);
+                    }else{
+                        done = false;
+                        step++;
+                    }
+                    break;
+                case (1):
+                    if(!done){
+                        done = robot.turnTo(steps[step], power);
+                    }else{
+                        done = false;
+                        step++;
+                    }
+
+                    break;
+                case (2):
+                    if(!done){
+                        done = robot.drive(0, steps[step], power);
+                    }else{
+                        done = false;
+                        step++;
+                    }
+
+                    break;
+                case (3):
+                    if(!done){
+                        done = robot.linearSlide.clawMove(steps[step]);
+                    }else{
+
+                        robot.stop();
+                        done = false;
+                        step++;
+                    }
+
+                    break;
+                case (4):
+                    if(!done){
+                        done = robot.drive(0, steps[step], power);
+                    }else{
+                        done = false;
+                        step++;
+                    }
+
+                    break;
+                case (5):
+                    if(!done){
+                        done = robot.turnTo(steps[step], power);
+                    }else{
+                        done = false;
+                        step++;
+                    }
+
+                    break;
+                case (6):
+                    if(!done){
+                        done = robot.linearSlide.clawMove(steps[step]);
+                    }else{
+
+                        robot.stop();
+                        done = false;
+                        step++;
+                    }
+
+                    break;
+                default:
+                    robot.stop();
+                    break;
+            }
         }
+
     }
 
 
